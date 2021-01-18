81abce78e8e6076580e579c65e52875fd446037f
vanlummelhuizen <michahulsbosch@gmail.com>
2021-01-18 11:58:57 +0100

    Add enrich data to CNGT metadata

--- python/extract_cngt_metadata.py
+++ python/extract_cngt_metadata.py
@@ -30,6 +30,19 @@ for row in sheet_sessions.iter_rows(min_row=2, max_col=5):
 
     sessions[row_as_list[1]] = dict(zip(header[0:1]+header[2:], row_as_list[0:1]+row_as_list[2:]))
 
+# Enriched sessions
+wb_enriched = load_workbook(sys.argv[2])
+sheet_sessions_enriched = wb_enriched['Sessies']
+header = [cell.value for cell in sheet_sessions_enriched['A1':'E1'][0]]
+for row in sheet_sessions_enriched.iter_rows(min_row=2, max_col=15):
+    row_as_list = [cell.value for cell in row]
+    session_id = row_as_list[1]
+    if session_id in sessions:
+        sessions[session_id]['duration'] = "{}:{}".format(row_as_list[12], row_as_list[13])
+        sessions[session_id]['is glossed'] = 'yes' if row_as_list[2] == 'ja' else 'no'
+        sessions[session_id]['is translated'] = 'yes' if row_as_list[3] == 'ja' else 'no'
+
+
 metadata = {
     'participants': participants,
     'sessions': sessions
